d10faf7e49853f7353e116d0b5abc2ffae75b958
taras shyshko <tarri13579@gmail.com>
2022-11-26 16:46:23 +0200

    Добавьте файлы проекта.

---
+++ Menu/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Menu.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Menu
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenOrder(object sender, EventArgs e)
+        {
+            if (OrderView.TranslationX >= 150)
+            {
+                Action<double> callback = InputTransparent => OrderView.TranslationX = InputTransparent;
+                OrderView.Animate("anim", callback, 150, 0, 16, 300, Easing.CubicInOut);
+            }
+        }
+
+        private void CloseOrder(object sender, EventArgs e)
+        {
+            if (OrderView.TranslationX == 0)
+            {
+                Action<double> callback = InputTransparent => OrderView.TranslationX = InputTransparent;
+                OrderView.Animate("anim", callback, 0, 150, 16, 300, Easing.CubicInOut);
+            }
+        }
+
+        private void AddOrder(object sender, EventArgs e)
+        {
+            var item = (sender as Button).BindingContext as Food;
+            vm.AddOrder(item);
+        }
+
+    }
+}
---
+++ Menu/ViewModel/MainViewModel.cs
@@ -0,0 +1,70 @@
+﻿using Menu.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace Menu.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+
+        public MainViewModel()
+        {
+            Orders = new ObservableCollection<Food>();
+            Foods = GetFoods();
+        }
+        public int OrderCount => Orders.Count;
+
+        public float TotalPrice => Orders.Sum(x => x.Price);
+
+        private ObservableCollection<Food> foods;
+
+        public ObservableCollection<Food> Foods
+        {
+            get { return foods; }
+            set
+            {
+                foods = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Food> orders;
+        public ObservableCollection<Food> Orders
+        {
+            get { return orders; }
+            set
+            {
+                orders = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(OrderCount));
+                OnPropertyChanged(nameof(TotalPrice));
+            }
+        }
+
+        private ObservableCollection<Food> GetFoods()
+        {
+            return new ObservableCollection<Food>
+            {
+                new Food{ Name = "Omelette Breakfast Dish", Image = "Omelette.png", Price = 13.99f, Description = "Яйця,сир твердий,ковбаса напівкопчена,олія оливкова,спеції,зелень"},
+                new Food{ Name = "California", Image = "California.jpg", Price = 22.59f, Description ="Водорості норі,олія,свіжий огірок,авокадо,слабосолений лосось,лайм"},
+                new Food{ Name = "Greek Salad with tomatoes", Image = "Greek.jpg", Price = 18.25f, Description ="Помідори,лайм,огірок,цибуля,оливки,сир Фета,орегано,спеції"},
+                new Food{ Name = "Cherry with pepper salad", Image = "Cherry.jpg", Price = 10.99f, Description ="Помідори чері,перець болгарський,стейк,салат,рукула,спецї"},
+            };
+        }
+
+        public void AddOrder(Food item)
+        {
+            if (item != null)
+            {
+                Orders.Add(item);
+                OnPropertyChanged(nameof(OrderCount));
+                OnPropertyChanged(nameof(TotalPrice));
+
+            }
+        }
+
+    }
+}
